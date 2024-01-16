6007ff22c14388f40bbbcf2b7b0f816f36927bb5
hornley <buendiaharley@gmail.com>
2024-01-16 14:54:23 +0800

    yey

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-function omsim() {
-    const h1 = document.getElementById("PageTitle");
-
-    h1.style.color = "powderblue";
+function opendata(form) {
+    var selectedIndex = form.elements['ProjectSelections'].selectedIndex;
+    var url = form.elements["ProjectSelections"].options[selectedIndex].value;
+    window.open(url);
 }
