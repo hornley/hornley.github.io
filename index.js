0fd186689f5ff8dc05e5a358937a2c7094309dd4
hornley <buendiaharley@gmail.com>
2024-01-19 00:53:20 +0800

    not just socials lol

--- index.js
+++
@@ -1,5 +0,0 @@
-function opendata(form) {
-    var selectedIndex = form.elements['ProjectSelections'].selectedIndex;
-    var url = form.elements["ProjectSelections"].options[selectedIndex].value;
-    window.open(url);
-}
