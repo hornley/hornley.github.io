dd3df62979e2e2d3afdf0fb501bab19b0ca0cbe7
hornley <buendiaharley@gmail.com>
2024-01-21 06:34:15 +0800

    Number System Converter

---
+++ Number_System/numsys.js
@@ -0,0 +1,79 @@
+let _ = new Array( 26 ).fill( 1 ).map( ( _, i ) => String.fromCharCode( 65 + i ) );
+
+const letters = new Map();
+const numbers = new Map();
+
+
+function convert() {
+    let number = document.getElementById("toConvert").value;
+    let base = document.getElementById("base").value;
+    let to = document.getElementById("to").value;
+
+    if (number == '' || base == '' || to == '') {
+        window.alert("Cannot continue with missing arguments!");
+        return;
+    }
+
+    if (parseInt(base) && parseInt(to)) {
+        base = parseInt(base);
+        to = parseInt(to);
+    } else {
+        window.alert("Your 'from' or 'to' input is not a number!s");
+        return;
+    }
+
+    if (to >= 36) {
+        window.alert("To Value: " + to + " might run into an error with remainders.");
+        return;
+    }
+
+    let _x = 10;
+
+    for (let letter in _) {
+        letters[_x] = _[letter];
+        numbers[_[letter]] = _x;
+        _x++;
+    }
+
+    if (base === 10) {
+        let x = fromDecimal(number, to);
+        console.log(x);
+    } else {
+        let x = toDecimal(number, base, to);
+        console.log(x);
+    }
+}
+
+function fromDecimal(number, to) {
+    let result = "";
+    m = parseInt(number);
+    q = m / to - (m % to / to);
+    if (m % to > 9) {
+        result = letters[m % to] + result;
+    } else {
+        result = m % to + result;
+    }
+    while (q != 0) {
+        m = q;
+        q = m / to - (m % to / to);
+        if (m % to > 9) {
+            result = letters[m % to] + result;
+        } else {
+            result = m % to + result;
+        }
+    }
+    return result;
+}
+
+function toDecimal(number, base, to) {
+    const length = number.length - 1;
+    let _ = 0;
+    let result = 0;
+
+    for (let i = length; i >= 0; i--) {
+        let currentNumber = (parseInt(number[_])) ? parseInt(number[_]) : numbers[_];
+        result = result + currentNumber * (base ** i);
+        _++;
+    }
+    return (to === 10) ? result : fromDecimal(result, to);
+}
--- TicTacToe/TicTacToe.js
+++ TicTacToe/TicTacToe.js
@@ -1,79 +1,85 @@
-let cur_turn = 1;
+let currentTurn = 1;
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-function play(block_number) {
+function play(blockNumber) {
     let player;
-    if (cur_turn == 0) {
+    if (currentTurn == 0) {
         return;
     }
-    const block = document.getElementById("Block" + block_number);
+    const block = document.getElementById("Block" + blockNumber);
 
-    if (block.style.backgroundColor == "red" || block.style.backgroundColor == "blue") {
+    if (block.children.length > 0) {
         return;
     }
 
-    if (cur_turn % 2 != 0) {
-        block.style.backgroundColor = "red";
+    let img = document.createElement("img");
+
+    if (currentTurn % 2 != 0) {
+        img.src = "x.png";
         player = "Player 1";
+        block.appendChild(img);
     } else {
-        block.style.backgroundColor = "blue";
+        img.src = "o.png";
         player = "Player 2";
+        block.appendChild(img);
     }
 
-    if (check_win()) {
+    if (checkWin()) {
         document.getElementById("h1Winner").innerHTML = player + " won!";
         return;
     }
     else {
-        cur_turn++;
+        currentTurn++;
     }
 }
 
 function restart() {
     let x = 1;
-    cur_turn = 1;
+    currentTurn = 1;
     document.getElementById("h1Winner").innerHTML = '';
     while (x < 10) {
         const block = document.getElementById("Block" + x);
-    
-        block.style.backgroundColor = "powderblue";
+        if (block.children.length > 0) {
+            block.removeChild(block.children[0]);
+        }
         x++;
     }
 }
 
-function check_win() {
+function checkWin() {
     let x = 1;
     let blocks = {};
+    const blockX = document.createElement('img'); blockX.src = 'x.png';
+    const blockO = document.createElement('img'); blockO.src = 'o.png';
     while (x < 10) {
         const block = document.getElementById("Block" + x);
-        blocks[x] = block.style.backgroundColor;
+        if (block.children.length > 0) {
+            let child = block.children[0];
+            blocks[x] = child.src;
+        }
         x++;
     }
 
     for (let i = 1; i < 4; i++) {
-        if (blocks[i] == "red" && blocks[i + 3] == "red" && blocks[i + 6] == "red" || 
-            blocks[i] == "blue" && blocks[i + 3] == "blue" && blocks[i + 6] == "blue") {
-                cur_turn = 0;
+    if (blocks[i] == blockX.src && blocks[i + 3] == blockX.src && blocks[i + 6] == blockX.src || 
+            blocks[i] == blockO.src && blocks[i + 3] == blockO.src && blocks[i + 6] == blockO.src) {
+                currentTurn = 0;
                 return true;
         }
     }
 
     for (let i = 1; i < 9; i += 3) {
-        if (blocks[i] == "red" && blocks[i + 1] == "red" && blocks[i + 2] == "red" ||
-            blocks[i] == "blue" && blocks[i + 1] == "blue" && blocks[i + 2] == "blue") {
-                cur_turn = 0;
+        if (blocks[i] == blockX.src && blocks[i + 1] == blockX.src && blocks[i + 2] == blockX.src ||
+            blocks[i] == blockO.src && blocks[i + 1] == blockO.src && blocks[i + 2] == blockO.src) {
+                currentTurn = 0;
                 return true;
         }
     }
 
-    if (blocks[1] == "red" && blocks[5] == "red" && blocks[9] == "red" ||
-        blocks[1] == "blue" && blocks[5] == "blue" && blocks[9] == "blue" ||
-        blocks[3] == "red" && blocks[5] == "red" && blocks[7] == "red" ||
-        blocks[3] == "blue" && blocks[5] == "blue" && blocks[7] == "blue") {
-                cur_turn = 0;
+    if (blocks[1] == blockX.src && blocks[5] == blockX.src && blocks[9] == blockX.src ||
+        blocks[1] == blockO.src && blocks[5] == blockO.src && blocks[9] == blockO.src ||
+        blocks[3] == blockX.src && blocks[5] == blockX.src && blocks[7] == blockX.src ||
+        blocks[3] == blockO.src && blocks[5] == blockO.src && blocks[7] == blockO.src) {
+                currentTurn = 0;
                 return true;
     }
 }
