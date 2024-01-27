1a4b2b78f2b432e4ba1d547e4c7f5bb75b1e4af9
hornley <buendiaharley@gmail.com>
2024-01-27 12:40:36 +0800

    Game Test mechanics

--- Game/game.js
+++ Game/game.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 let spawnLineY = 25;
-let spawnRate = 1;
+let spawnRate = 15;
 let objects = [];
 let lastSpawn = -1;
 let startTime = Date.now();
@@ -23,8 +23,24 @@ function spawnRandomObject() {
         x: Math.random() * (canvas.width - 30) + 15,
         y: spawnLineY,
 
-        collide: function(mouseX, mouseY) {
-            if (mouseX === Math.floor(this.x) && mouseY === Math.floor(this.y)) {
+        collide: function(object) {
+            let myleft = this.x - 1;
+            let myright = this.x + 1;
+            let mytop = this.y - 1;
+            let mybottom = this.y + 1;
+            let otherleft = object.x - object.width / 2 - 3;
+            let otherright = object.x + object.width / 2 + 3;
+            let othertop = object.y - object.height / 2;
+            let otherbottom = object.y + object.height / 2;
+            if ((mybottom >= othertop) && (myright <= otherright) && (myleft >= otherleft) && (mytop <= otherbottom)) {
+                return true;
+            }
+            return false;
+        },
+
+        crash: function() {
+            const bottom = canvas.height - 5;
+            if (this.y >= bottom) {
                 return true;
             }
             return false;
@@ -36,6 +52,13 @@ function spawnRandomObject() {
 
 function animate() {
     let time = Date.now();
+    let player_object = {
+        type: "black",
+        x: canvas.width / 2,
+        y: canvas.height / 2,
+        width: 50,
+        height: 50
+    }
 
     // see if its time to spawn a new object
     if (time > (lastSpawn + spawnRate)) {
@@ -58,22 +81,45 @@ function animate() {
     ctx.lineTo(canvas.width, spawnLineY);
     ctx.stroke();
 
+    canvas.addEventListener('mousemove', function (e) {
+        canvas.x = e.offsetX;
+        canvas.y = e.offsetY;
+    })
+    canvas.addEventListener('mousedown', function (e) {
+        canvas.x = e.pageX;
+        canvas.y = e.pageY;
+    })
+    canvas.addEventListener('mouseup', function (e) {
+        canvas.x = false;
+        canvas.y = false;
+    })
+    
+    if (canvas.x && canvas.y) {
+        player_object.x = canvas.x;
+        player_object.y = canvas.y;
+    }
+
+    ctx.beginPath();
+    ctx.rect(player_object.x - player_object.width / 2, player_object.y - player_object.height / 2, player_object.width, player_object.height);
+    ctx.stroke();
+
     // move each object down the canvas
     for (let i = 0; i < objects.length; i++) {
         let object = objects[i];
-        object.y += 0.5;
-        ctx.beginPath();
-        ctx.arc(object.x, object.y, 2, 0, Math.PI * 2);
-        ctx.closePath();
-        ctx.fillStyle = object.type;
-        ctx.fill();
+        object.y += 1;
+        if (object.collide(player_object) || object.crash()) {
+            objects.splice(i, 1);
+        } else {
+            ctx.beginPath();
+            ctx.arc(object.x, object.y, 2, 0, Math.PI * 2);
+            ctx.closePath();
+            ctx.fillStyle = object.type;
+            ctx.fill();
+        }
     }
 }
 
 function showCoords(event) {
     let x = event.offsetX;
     let y = event.offsetY;
-    for (let i = 0; i < objects.length; i++) {
-        objects[i].collide(x, y);
-    }
 }
