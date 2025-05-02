09c45c0038b4587ac9176b9abfbfe938e019ca55
hornley <buendiaharley@gmail.com>
2025-05-02 14:45:34 +0800

    fix updateGrade() and gradeEquivalent

--- Calculators/TIP-Grade-Calculator/script.js
+++ Calculators/TIP-Grade-Calculator/script.js
@@ -32,7 +32,7 @@ const gradeText = document.getElementById("grade")
 function updateGrade() {
     let total_cs_percentage = 0
 
-    grades[currentPeriod]["Class Standing"] = {"Grade": 0, "Modified": grades[currentPeriod]["Class Standing"]["Modified"]}
+    grades[currentPeriod]["Class Standing"] = {"Grade": 0}
 
     CSContainer.childNodes.forEach((node) => {
         if (node.className === "cs-factor") {
@@ -52,12 +52,11 @@ function updateGrade() {
             })
 
             grades[currentPeriod]["Class Standing"][nodeName]["Percentage"] = round(parseFloat(node.firstChild.childNodes[2].value))
-            grades[currentPeriod]["Class Standing"][nodeName]["Grade"] = round(grade / (Object.keys(grades[currentPeriod]["Class Standing"][nodeName]).length - 2))
+            grades[currentPeriod]["Class Standing"][nodeName]["Grade"] = round(grade / (Object.keys(grades[currentPeriod]["Class Standing"][nodeName]).length - 2)) || 0
             node.firstChild.lastChild.value = grades[currentPeriod]["Class Standing"][nodeName]["Grade"]
             total_cs_percentage += parseFloat(node.firstChild.childNodes[2].value) || 0
         }
     })
-
     if (0 >= total_cs_percentage <= 100) {
         grades[currentPeriod]["Class Standing"]["Grade"] = 0
         Object.keys(grades[currentPeriod]["Class Standing"]).forEach((factor) => {
@@ -65,7 +64,7 @@ function updateGrade() {
                 grades[currentPeriod]["Class Standing"]["Grade"] += grades[currentPeriod]["Class Standing"][factor]["Grade"] * grades[currentPeriod]["Class Standing"][factor]["Percentage"] / 100
             }
         })
-        grades[currentPeriod]["Class Standing"]["Grade"] = round(grades[currentPeriod]["Class Standing"]["Grade"])
+        grades[currentPeriod]["Class Standing"]["Grade"] = round(grades[currentPeriod]["Class Standing"]["Grade"]) || 0
         classStandingGrade.value = grades[currentPeriod]["Class Standing"]["Grade"]
 
         let CS = round(parseFloat(classStandingGrade.value)) || 0
@@ -78,11 +77,12 @@ function updateGrade() {
         else if (currentPeriod === "Midterm")
             grades[currentPeriod]["Grade"] = round(1/3*(grades["Prelim"]["Grade"]) + 2/3*(CS/2 + ME/2))
         else
-            grades[currentPeriod]["Grade"] = round(1/3*(grades["Midterm"]["Grade"]) + 2/3*(CS/2 + ME/2))
+            grades[currentPeriod]["Grade"] = Math.round(1/3*(grades["Midterm"]["Grade"]) + 2/3*(CS/2 + ME/2))
 
-        gradeText.innerText = grades[currentPeriod]["Grade"]
+        gradeText.innerText = grades[currentPeriod]["Grade"] + " (" + gradeEquivalent(grades[currentPeriod]["Grade"]).toFixed(2) + ")"
     }
 }
+
 
 setInterval(updateGrade, 500)
 
@@ -319,6 +319,33 @@ function deleteSubCategory(node) {
 
 function round(num) {
     return Math.round((num + Number.EPSILON) * 100)/100
+}
+
+function gradeEquivalent(grade) {
+    let equivalentGrade;
+
+    if (94 <= grade && grade <= 100)
+        equivalentGrade = 1.00;
+    else if (88.5 <= grade && grade < 94)
+        equivalentGrade = 1.25;
+    else if (83 <= grade && grade < 88.5)
+        equivalentGrade = 1.50;
+    else if (77.5 <= grade && grade < 83)
+        equivalentGrade = 1.75;
+    else if (72 <= grade && grade < 77.5)
+        equivalentGrade = 2.00;
+    else if (65.5 <= grade && grade < 72)
+        equivalentGrade = 2.25;
+    else if (61 <= grade && grade < 65.5)
+        equivalentGrade = 2.50;
+    else if (55.5 <= grade && grade < 61)
+        equivalentGrade = 2.75;
+    else if (50 <= grade && grade < 55.5)
+        equivalentGrade = 3.00;
+    else if (0 <= grade && grade < 50)
+        equivalentGrade = 5.00;
+    
+    return equivalentGrade
 }
 
 function updateCategories(categoriesCount) {
